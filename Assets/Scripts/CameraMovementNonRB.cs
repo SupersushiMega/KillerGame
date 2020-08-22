23abb5589f314da1c3322f1907739d46051ef7cd
SupersushiMega <sascha.rutz@outlook.com>
2020-08-22 14:06:14 +0200

    Changed to CharacterController

---
+++ Assets/Scripts/CameraMovementNonRB.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovementNonRB : MonoBehaviour
+{
+    public float Sensitivity = 100f;
+    public Transform Body;
+    float Rotx = 0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * Sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * Sensitivity * Time.deltaTime;
+        Rotx -= mouseY;
+
+        Rotx = Mathf.Clamp(Rotx, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(Rotx, 0f, 0f);
+        Body.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/PlayerMovementNonRB.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementNonRB : MonoBehaviour
+{
+    public float Gravity = 9.81f;
+    public float speed = 12f;
+    public float JumpHeight = 1f;
+
+    public Transform GroundCheck;
+    public float GroundDistance = 0.4f;
+    public LayerMask GroundMask;
+
+    public CharacterController Controller;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(GroundCheck.position, GroundDistance, GroundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                velocity.y = Mathf.Sqrt(JumpHeight * -2f * -Gravity);
+            }
+            else
+            {
+                velocity.y = -2f;
+            }
+        }
+
+        velocity.y += -Gravity * Time.deltaTime;
+
+        Controller.Move(velocity * Time.deltaTime);
+
+        float x = Input.GetAxis("Vertical");
+        float z = Input.GetAxis("Horizontal");
+
+        Vector3 move = transform.right * z + transform.forward * x;
+
+        Controller.Move(move * speed * Time.deltaTime);
+
+    }
+}
