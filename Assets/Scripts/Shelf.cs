44dbdf92ab0948d2a7b15976465238336d064797
SupersushiMega <sascha.rutz@outlook.com>
2020-08-25 14:09:14 +0200

    Shelf added

---
+++ Assets/Scripts/Shelf.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shelf : MonoBehaviour
+{
+    public int Space = 10;
+    public int ItemCount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
