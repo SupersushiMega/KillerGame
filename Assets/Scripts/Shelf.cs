e452b008588ee5d9697b381b7d3db8174bd4588c
SupersushiMega <sascha.rutz@outlook.com>
2020-08-26 13:44:21 +0200

    Visual Shelf Inventory Levelel added

--- Assets/Scripts/Shelf.cs
+++ Assets/Scripts/Shelf.cs
@@ -4,18 +4,42 @@ using UnityEngine;
 
 public class Shelf : MonoBehaviour
 {
-    public int Space = 10;
+    List<GameObject> Cupcakes = new List<GameObject>();
+    public float Space = 20;
+    [Range(0f, 20f)]
     public int ItemCount = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach (Transform child in transform)
+        {
+            Cupcakes.Add(child.gameObject);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        short i;
+        if (ItemCount != 0)
+        {
+            for (i = 0; i <= (ItemCount / Space) * Cupcakes.Count; i++)
+            {
+                Cupcakes[i].SetActive(true);
+            }
+        }
+
+        else
+        {
+            i = 0;
+        }
+
+#pragma warning disable CS1717 // Die Zuweisung wurde für dieselbe Variable durchgeführt.
+        for (i = i; i <= Cupcakes.Count; i++)
+#pragma warning restore CS1717 // Die Zuweisung wurde für dieselbe Variable durchgeführt.
+        {
+            Cupcakes[i].SetActive(false);
+        }
     }
 }
