b5d961d9cfcae1377a9e518d8fe3dae5e46664e4
SupersushiMega <sascha.rutz@outlook.com>
2020-09-04 21:04:01 +0200

    Added dynamic Navmesh using NavMeshComponents from Unity-Technologies

---
+++ Assets/Scripts/RoomDetection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RoomDetection : MonoBehaviour
+{
+    public GameObject Wall;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "ShopFloor")
+        {
+            
+            Wall.SetActive(false);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "ShopFloor")
+        {
+            Wall.SetActive(true);
+        }
+    }
+}
